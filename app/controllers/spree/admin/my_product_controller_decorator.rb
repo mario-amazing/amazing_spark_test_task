0553e187f27fc6774bdd04b650fcdb4e68d15d9a
mario-amazing <marian1amazing@gmail.com>
2019-08-13 22:38:02 +0300

    Add uploaded csv file table on index

--- app/controllers/spree/admin/my_product_controller_decorator.rb
+++ app/controllers/spree/admin/my_product_controller_decorator.rb
@@ -3,6 +3,7 @@ module Spree
     module MyProductDecorator
       def upload_csv_files
         @uploaded_file = UploadedFile.new(model_type: Spree::Product.name)
+        @collection = UploadedFile.all
       end
     end
   end
